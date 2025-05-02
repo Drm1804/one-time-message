f73f9513d80ed24d82905ffac03e19fed2038ad9
Alex Four <>
2025-05-02 16:01:22 +0200

    add express health ckecker

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { conf } from '../config.js';
 import { logger } from './utils/logger.js';
 import { initBot } from './services/bot/bot.js';
 import { initDatabase } from './utils/database.js';
+import express from 'express';
 
 (async (): Promise<void> => {
   const log = logger('Main');
@@ -12,4 +13,16 @@ import { initDatabase } from './utils/database.js';
   } = conf;
   await initDatabase(firebase, email, password);
   await initBot(conf.botToken);
+
+  const app = express();
+  app.use(express.json());
+
+  app.get('/ping', (_, res) => {
+    res.json({ message: 'Hello, World!' });
+  });
+
+  // Start the server on port 3000
+  app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+  });
 })();
